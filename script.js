a0a57807f8774a8975c8fd364b64752078c33117
Josh Broughton <spikebroughton@aol.com>
2020-12-03 21:33:58 -0500

    Weather Dashboard

--- script.js
+++ script.js
@@ -8,9 +8,11 @@
 // var currentHumidity=$("#humidity");
 // var currentWSpeed=$("#wind-speed");
 // var sCity=[];
+var lat = "";
+var lon = "";
 
 // //Global veriabal
-// var userCity="";
+ var userCity="";
 
 // function find(c){
 //     for (var i=0; i<sCity.length; i++){
@@ -22,14 +24,17 @@
 // }
 
 var APIKey="&appid=74e0dbda9a9ba1f2046e6d81f6487cfa";
-var quearyurl="http://api.openweathermap.org/data/2.5/forecast?q=";
+var quearyurl="http://api.openweathermap.org/data/2.5/weather?q=";
 var imperialUnits = "&units=imperial";
 //Search button onClick event
-//$('#search-button').on("click", function () {
-    //userCity = $("#city").val().trim();
+$('#search-button').on("click", function () {
+    userCity = $("#city-search").val().trim();
+    console.log(userCity);
     var newUrl= quearyurl + userCity +imperialUnits + APIKey;
+    var uvquaryurl="http://api.openweathermap.org/data/2.5/uvi?Lat="+lat+'&lon='+lon+APIKey
     runqueary(newUrl)
-//});
+    
+});
 // $('#search-button').on("click", function(){
 // //Get City Value from user input
 // userCity= $("#city-search").val().trim();
@@ -42,10 +47,55 @@ function runqueary(quaryurl){
 
     }).done(function (weatherdata){
         console.log(weatherdata)
-        console.log(weatherdata.list[0].main.temp)
-        console.log(weatherdata.list[0].main.humidity)
-        console.log(weatherdata.list[0].main.uv)
-        console.log(weatherdata.list[0].wind.speed)
+        var weatherinfo=$("<div>");
+        weatherinfo.addClass("weather");
+        $("#current-city").append(weatherinfo);
+
+        $(".weather").append("<h2>"+weatherdata.name+"</h2>");
+        $(".weather").append("<h2>"+weatherdata.main.temp+"</h2>");
+        $(".weather").append("<h2>"+weatherdata.main.humidity+"</h2>");
+        $(".weather").append("<h2>"+weatherdata.wind.speed+"</h2>");
+        lat=weatherdata.coord.lat
+        lon=weatherdata.coord.lon 
+        console.log(lon)
+        console.log(lat)
+        runuvqueary(lat,lon)
+        futureeweather(lat,lon)
+
+        
+        //console.log(weatherdata.list[0].main.temp)
+        //console.log(weatherdata.list[0].main.humidity)
+        //console.log(weatherdata.list[0].wind.speed)
 
     })
 }
+
+//var uvquaryurl="http://api.openweathermap.org/data/2.5/uvi?Lat="+lat+'&lon='+lon+'&appid='+APIKey
+
+function runuvqueary(lat,lon){
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/uvi/forecast?lat="+lat+'&lon='+lon+APIKey,
+    method: "GET"
+})
+.done(function(weatherdata){
+    console.log(weatherdata)
+    var weatherinfo=$("<div>");
+    weatherinfo.addClass("weather");
+$("#current-city").append(weatherinfo);
+$(".weather").append("<h2>"+weatherdata[0].value+"</h2>");
+})}
+
+//var i;
+//for (i = 0; i < weatherdata.length; i++) {
+  //text += weatherdata.list[i] + "<br>";
+//}
+function futureeweather(lat,lon){
+$.ajax({
+url: "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+'&lon='+lon+APIKey,
+method: "GET"
+})
+.done(function(forecastdata){
+    $(".weather").append("<h2>"+forecastdata[0].futureeweather+"</h2>")
+console.log(forecastdata)
+})}
+
